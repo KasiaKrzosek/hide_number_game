f691bb02834a60711488605fdfd080646847020b
kasia k <krzosek.kasia@gmail.com>
2020-12-07 18:22:21 +0100

    good sorting list

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,22 +1,26 @@
 
 import random
 import time
+import math
 
 named_tuple = time.localtime() # get struct_time
 time_string = time.strftime("%Y/%m/%d | %H:%M", named_tuple)
 #print(time_string)
-version = input("Hide numbers game / ukryte liczby\n\nPlease choose version write 'En'/ \nProsze wybierz wersje napisz 'Pl': ") #choose version EN/PL
+version = input("Hide numbers game / ukryte liczby\n\nPlease choose version write \
+'En'/ \nProsze wybierz wersje napisz 'Pl': ") #choose version EN/PL
 version = version.capitalize() #large input string does not matter
 print()
 while (version != "En") and ( version != "Pl"): #for sure to right answer
     version = input("error, error, please write 'En'/ wykryto blad, prosze wpisz 'Pl': ") 
+    print()
     version = version.capitalize() 
 
-
+#english version
 if version == "En":
     answer = "yes"
-   
-    print ("Hello,\nHow are You? Game choose randomly number, and your job is gussed it.\nIf You need a help (just 2 times avilable) write 'help'.\nHave fun ;)") #welcome
+    print ("Hello,\nHow are You? Game choose randomly number, \
+and your job is gussed it.\nIf You need a help \
+(just 2 times avilable) write 'help'.\nHave fun ;)") #welcome
    
     while answer == "yes": #loop to restart game
 #start setup         
@@ -32,8 +36,9 @@ if version == "En":
         WN = 0
         WriteNumber = []       
         SaveResults = 0
-        print ()
-        print ("Try to guess integer from range", a, "to", b)# print the range
+        Words = []
+
+        print ("\nTry to guess integer from range", a, "to", b)# print the range
         start_time = time.time()
 #check number WN        
         while WN != SN:
@@ -71,8 +76,8 @@ if version == "En":
 #Congratulation
             if(WN == SN):   #loop repeat game and good/bad number
                 elapsed_time = time.time() - start_time
-                print()
-                print("Great! You got it!\nYour time is", int(elapsed_time), "seconds\nYou writed:", WriteNumber, "\nYou guess after",Guessing,"guess")                
+                elapsed_time = int(elapsed_time)
+                print("\nGreat! You got it!\nYour time is", elapsed_time, "seconds\nYou writed:", WriteNumber, "\nYou guess after",Guessing,"guess")                
 #save results
                 SaveResults = input("Do You want to save your results? Please, write yes / no ") #ask
                 SaveResults = SaveResults.lower()
@@ -86,32 +91,47 @@ if version == "En":
                 else:
     #name, data
                     Name = input("Please, write your name: ")
-                    Save = ( str(Guessing) + " | " + str(elapsed_time) + " | "  + Name + " | " + time_string + " | " + str(SN)) 
+                    Save = ( str(Guessing) + " | " + str(elapsed_time)\
+                       + " | "  + Name + " | " + time_string + " | " + str(SN)) 
                     Save = str(Save)
                     Save = Save + ("\n")
-    #save in txt
-                    f = open(filepath, "a")
-                    f.write(Save) #write in the end thank "a"
-                    f.close()
+    #save in file
+                    with open(filepath, "a") as f:
+                        f.write(Save) #write in the end thank "a"
     #list
-                    f = open(filepath, "r")
-                    lines = f.readlines() #create list
-                    Place = 0
-                    lines.sort() #sort list
-                    #is some problem with sort
-                    Place = lines.index(Save)+ 1 #jakie miejsce
-                    print()
-                    print("You have", Place , "place among", len(lines), "results") #wyswietla ktore miejsce z ilu wynikow
-                    f.close()
+                    with open(filepath, "r") as f:
+                        lines = f.readlines() #create list
+                        Place = 0
+
+                        for line in lines:
+                            line = line[:-1]
+                            SplitLine = line.split(" | ")
+                            newLine = [[int(SplitLine[0])] + [int(SplitLine[1])] + SplitLine[2:]]
+                            Words.extend(newLine)
+
+                        Save = []
+                        time_string = time_string.split(" | ")
+                        Save = [Guessing, int(elapsed_time)]
+                        Save.append (Name)
+                        Save.append (time_string[0])
+                        Save.append (time_string[1])
+                        Save.append(str(SN))
+                        Words.sort()        #sort list
+                        number = 0 
+
+                        for Result in Words: 
+                            number +=1
+
+                    Place = Words.index(Save)+ 1 #how place
+                    print("\nYou have", Place , "place among", number, "results")                    
 #ranking 1-10
-                print()
-                print("10 the best results is: \n") #ranking 1-10
-                print("Guess | Time | Name | Day | Hour |  Hide number")
-                for Result in lines[0:10]: 
-                    print(Result, end="")
+                print("\n10 the best results is: \nplace.....Guess | Time | Name | Day | Hour |  Hide number") #ranking 1-10
+                number = 0
+                for result in Words[0:10]:
+                    number +=1
+                    print(str(number) + "....." + str(result[0]) + " | " + str(result[1]) + " | " + str(result[2]) + " | " + str(result[3]) + " | " + str(result[4]) + " | " + str(result[5]))                                       
 #restart
-                print()
-                answer = input("Do You want to repeat? Write yes or no: ") 
+                answer = input("\nDo You want to repeat? Write yes or no: ") 
                 answer = answer.lower()
 
                 while (answer != "yes") and (answer != "no"): #for sure to right answer
@@ -133,25 +153,26 @@ if version == "En":
 #wersja polska
 elif (version == "Pl"):
     odpowiedz = "tak" #gra dziala, na koniec pytanie czy uruchomic ponownie
-
-    print ("Czesc ;)\nGra polega na odgadnieciu wylosowanej losowo liczby.\nJesli potrzebujesz pomocy (tylko dwa razy mozliwe) napisz 'pomoc'.\nMilej zabawy ;)") 
+    print ("Czesc ;)\nGra polega na odgadnieciu wylosowanej losowo liczby.\n\
+Jesli potrzebujesz pomocy (tylko dwa razy mozliwe) napisz 'pomoc'.\nMilej zabawy ;)") 
     
     while odpowiedz == "tak":
 #ustawienia wstepne
         a = random.randint(1,500)
         b = random.randint(500,1000)   
         SL = random.randint(a, b) #SekretnaLiczba
-        print (SL)
+        #print (SL)
         SciezkaPliku = "wyniki.txt"
         imie = 0
         WpisaneLiczby = []
-        PoczatekCzasu = time.time()
         WL = 0
         odgadywanie = 0
         LiczPomoc = 0
         ZapisWyniku = 0
-        print("\nsprobuj zgadnac liczbe calkowita z zakresu od", a, "do", b)
+        wyrazy = []
 
+        print("\nsprobuj zgadnac liczbe calkowita z zakresu od", a, "do", b)
+        PoczatekCzasu = time.time()
 # sprawdzLiczbe(WL)          
         while WL != SL:  
             WL = input("sprobuj zgadnac liczbe: ") #WpisanaLiczba  
@@ -190,8 +211,8 @@ elif (version == "Pl"):
 #gratulacje
             if(WL == SL):
                 KoniecCzasu = time.time() - PoczatekCzasu
-                print()
-                print("super! Udalo Ci sie!\nzajelo Ci to", KoniecCzasu, "sekund\nwpisales nastepujace liczby:", WpisaneLiczby,"\nzgadles po",odgadywanie,"probach")   
+                KoniecCzasu = int(KoniecCzasu)
+                print("\nsuper! Udalo Ci sie!\nzajelo Ci to", KoniecCzasu, "sekund\nwpisales nastepujace liczby:", WpisaneLiczby,"\nzgadles po",odgadywanie,"probach")   
 #zapis wyniku
                 ZapisWyniku = input("czy chcesz zapisac swoj wynik? napisz tak / nie ") #pytanie
                 ZapisWyniku = ZapisWyniku.lower()
@@ -204,33 +225,47 @@ elif (version == "Pl"):
                     print("nie zapisano wyniku")
                 else:
     #imie, dane
-                    imie = input("wpisz swoje imie: ")
-                    KoniecCzasu = int(KoniecCzasu)
+                    imie = input("wpisz swoje imie: ")                    
                     zapis = ( str(odgadywanie) + " | " + str(KoniecCzasu) + " | "  + imie + " | " + time_string + " | " + str(SL)) 
                     zapis = str(zapis)
-                    zapis = zapis + ("\n")
-                     
-                    #imie1 = imie + "\n" #przechodzi do kolejnej linijki
+                    zapis = zapis + ("\n")                    
     #zapisuje w pliku
-                    S = open(SciezkaPliku, "a", encoding="utf-8") #encoding="utf-8" - by polskie znaki czytalo
-                    S.write(zapis) #dopisuje na koncu dzieki "a"
-                    S.close()
+                    with open(SciezkaPliku, "a", encoding="utf-8") as S: #encoding="utf-8" - by polskie znaki czytalo
+                        S.write(zapis) #dopisuje na koncu dzieki "a"
     #lista
-                    S = open(SciezkaPliku, "r")
-                    wiersze = S.readlines() #utworz liste
-                    miejsce = 00
-                    wiersze.sort() #sortuje liste
-                    #sortowanie ma problem nie bierze liczby jako liczby 100 jest mniejsze niz 2
-                    miejsce = wiersze.index(zapis)+ 1 #jakie miejsce
-                    print()
-                    print("masz", miejsce , "miejsce sposrod", len(wiersze), "wynikow") #wyswietla ktore miejsce z ilu wynikow
-                    S.close()
+                    with open(SciezkaPliku, "r") as S:
+                        wiersze = S.readlines() #utworz liste
+                        miejsce = 0
+
+                        for wiersz in wiersze:
+                            wiersz = wiersz[:-1]
+                            rozdzielonyWiersz  = wiersz.split(" | ")
+                            nowyWiersz = [[int(rozdzielonyWiersz[0])] + [int(rozdzielonyWiersz[1])] + rozdzielonyWiersz[2:]]
+                            wyrazy.extend(nowyWiersz)
+
+                        zapis = []
+                        time_string = time_string.split(" | ")
+                        zapis = [ odgadywanie, int(KoniecCzasu)]
+                        zapis.append (imie)
+                        zapis.append (time_string[0])
+                        zapis.append (time_string[1])
+                        zapis.append(str(SL))
+                        wyrazy.sort()    
+                        liczba = 0     
+
+                        for wynik in wyrazy: 
+                            liczba +=1
+                            
+                        miejsce = wyrazy.index(zapis) + 1 #jakie miejsce
+                        print("\nmasz", miejsce , "miejsce sposrod", liczba, "wynikow") #wyswietla ktore miejsce z ilu wynikow
 #ranking 1-10
-                print()
-                print("10 najlepszych wynikow to: \n") #pokazuje ranking 1-10
-                print("liczba prob | czas | imie | data | godzina |  ukryty numer")
-                for wynik in wiersze[0:10]: 
-                    print(wynik, end="")                      
+                print("\n10 najlepszych wynikow to: \n\nmiejsce.....liczba prob | czas | imie | data | godzina |  ukryty numer") #pokazuje ranking 1-10
+                liczba = 0
+
+                for wynik in wyrazy[0:10]: 
+                    liczba += 1
+                    print( str(liczba) + "....." + str(wynik[0]) + " | " + str(wynik[1]) + " | " + str(wynik[2])\
+                    + " | " + str(wynik[3]) + " | " + str(wynik[4]) + " | " + str(wynik[5]))                                       
 #ponowna gra
                 print()
                 odpowiedz = input("czy chcesz zagrac ponownie? Wpisz tak lub nie: ") 
@@ -250,6 +285,8 @@ elif (version == "Pl"):
             elif(WL > SL):
                 print ("blad, ukryta liczba jest mniejsza, sprobuj ponownie")  
                    
-                
-               
-               
+"""  
+wszystkie wyniki
+                        for wynik in newS: 
+                            print(wynik)
+"""
